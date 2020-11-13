f85713d7739070aa5ef4bc347112a38891ca0098
Prog Mai <progmaimairaming@gmail.com>
2020-11-13 13:45:17 +0000

    user pages

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -19,11 +19,4 @@ class Authenticate extends Middleware
         }
     }
     
-    public function handle($request,Closure $next,$guard = null)
-    {
-        if(Auth::guard($guard)->check()) {
-            return redirect(RouteServiceProvider::HOME);
-        }
-        return $next($request);
-    }
 }
