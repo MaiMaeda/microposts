8b5ce275e6d5b7be2f833e1f4cdbb248e2d4b5e3
Prog Mai <progmaimairaming@gmail.com>
2020-11-16 04:38:19 +0000

    post

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
+    @if(\Auth::check())
+    <div class="row">
+        <aside class="col-sm-4">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">{{\Auth::user()->name}}</h3>
+                </div>
+                <div class="card-body">
+                    <img class="rounded img-fluid" src="{{Gravatar::get(Auth::user()->email, ['size' => 500]) }}" alt="">
+                </div>
+            </div>
+        </aside>
+        <div class="col-sm-8">
+            @include('microposts.form')
+            @include('microposts.microposts')
+        </div>
+    </div>
+    @else
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to the Microposts</h1>
@@ -8,4 +26,5 @@
             {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
+    @endif
 @endsection
